509dfa7622bd4948d4a8a4133704e60c03743fea
wgdadmin <kunidong@kunidongmatoMac-mini.local>
2012-08-21 06:50:42 +0800

    first commit'

---
+++ MoveButtonTest/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  MoveButtonTest
+//
+//  Created by wgdadmin on 12-8-21.
+//  Copyright (c) 2012年 wgdadmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    UIButton *sharp1;
+    
+}
+@end
---
+++ MoveButtonTest/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  MoveButtonTest
+//
+//  Created by wgdadmin on 12-8-21.
+//  Copyright (c) 2012年 wgdadmin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    sharp1 = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+    sharp1.backgroundColor = [UIColor redColor];
+    [sharp1 addTarget:self action:@selector(click:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:sharp1];
+    [sharp1 release];
+    [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(move) userInfo:nil repeats:YES];
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+-(void)click:(UIButton *)sender
+{
+    NSLog(@"click");
+}
+
+-(void)move
+{
+    static int y = 0;
+    y+=10;
+    if(y >430)
+    {   y= 0;
+    }
+    sharp1.frame = CGRectMake(165, y, 50, 50);
+}
+@end
